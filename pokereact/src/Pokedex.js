bb4746cb94a150f5e404303c9d4b055d02bc17a7
Andres Lira <andres.lira20@gmail.com>
2022-07-05 01:34:08 -0500

    search box

--- pokereact/src/Pokedex.js
+++ pokereact/src/Pokedex.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useFetch } from './hooks/useFetch'
 import { CardView } from './components/CardView'
 import { ListView } from './components/ListView'
 import './Pokedex.css'
 
 const Pokedex = () => {
+  const [pokedex, setPokedex] = useState([])
   const [gridMode, setGridMode] = useState(false)
   const [search, setSearch] = useState('')
+  const [pokedexComplete, isLoading, error] = useFetch('/pokedex')
+
+  useEffect(() => {
+    setPokedex(pokedexComplete)
+    setSearch('')
+  }, [isLoading])
 
   const handleSearch = event => {
     setSearch(event.target.value.toLowerCase())
@@ -16,7 +23,12 @@ const Pokedex = () => {
     setGridMode(!gridMode)
   }
 
-  const [pokedex, isLoading, error] = useFetch('/pokedex')
+  useEffect(() => {
+    const pkmFilter = pokedexComplete.filter(pokemon => {
+      return pokemon.nombre.toLowerCase().includes(search)
+    })
+    setPokedex(pkmFilter)
+  }, [search])
 
   if (isLoading) return <p>Loading...</p>
   if (error) return <p>{error}</p>
